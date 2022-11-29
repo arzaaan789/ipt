5a36a58ef94eac66b6fe2fb61b75f6e0920a58ad
Khizir Farrukh <khizirfarrukh@outlook.com>
2022-11-29 16:06:36 +0500

    x

--- CourseRegistration-MVC/CourseRegistration/Models/AdminLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CourseRegistration.Models
-{
-    public class AdminLogin
-    {
-        public string ID { get; set; }
-        public string Password { get; set; }
-    }
-}
--- CourseRegistration-MVC/CourseRegistration/Models/StudentLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CourseRegistration.Models
-{
-    public class StudentLogin
-    {
-        public string studentID { get; set; }
-        public string password { get; set; }
-    }
-}
